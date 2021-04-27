77684aa02167a7f9c2c1d3664b814d1a6b983fad
sergey <dn9wkalolz@gmail.com>
2021-04-27 12:31:02 +0300

    refactor: change project structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { compose, createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { rootReducer } from './store/rootReducer';
 
-// @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(),
 );
 
 const app = (
